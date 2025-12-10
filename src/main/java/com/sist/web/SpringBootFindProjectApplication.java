72e70559ac1f3fa36e0ede1791b817f98130a44c
hongeunseo321 <hhyyss5520@gmail.com>
2025-12-10 15:28:04 +0900

    commit

--- src/main/java/com/sist/web/SpringBootFindProjectApplication.java
+++ src/main/java/com/sist/web/SpringBootFindProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.sist.web.mapper")
 public class SpringBootFindProjectApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/sist/web/SpringBootFindProjectApplicationTests.java
+++ src/test/java/com/sist/web/SpringBootFindProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.sist.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringBootFindProjectApplicationTests {
 
 	@Test
